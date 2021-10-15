97afd7f15ecf8561bed0bb3a87095d00a6e6ae16
Dinesh Yadav <41871046+dineshyadav19@users.noreply.github.com>
2021-10-15 11:02:42 +0530

    Updated index.js

--- Polyfill/index.js
+++ Polyfill/index.js
@@ -1,20 +1,20 @@
 // Polyfill for reverse
 
-// Array.prototype.myReverse = function() {
-//     let start = 0;
-//     let end = arr.length - 1;
-//     while(start < end) {
-//         let temp = arr[start]
-//         arr[start] = arr[end]
-//         arr[end] = temp;
-//         start++;
-//         end--;
-//     }
-// }
+Array.prototype.myReverse = function() {
+    let start = 0;
+    let end = arr.length - 1;
+    while(start < end) {
+        let temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
 
-// const arr = [1,2,3,4]
-// arr.myReverse()
-// console.log(arr)
+const arr = [1,2,3,4]
+arr.myReverse()
+console.log(arr)
 
 
 //Polyfil for join
@@ -40,4 +40,4 @@ Array.prototype.myJoin = function(param) {
 const arr = [1,2,3]
 console.log(arr.myJoin(' - '))
 
-//polyfil for split
+
