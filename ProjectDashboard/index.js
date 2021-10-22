bd35c6b6a52e5142f72acd92b03931fb783ea56f
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2021-10-22 21:56:15 +0530

    Almost Done

--- ProjectDashboard/index.js
+++ ProjectDashboard/index.js
@@ -5,108 +5,119 @@ const Data = [
       projectName: "New Admin Design",
       startDate: "02/5/2019",
       status: "Completed",
-      members: [
-          {
-              //name: "Design",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-1.jpg"
-          }
-      ],
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-1.jpg"
+    //   [
+    //       {
+    //           //name: "Design",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-1.jpg"
+    //       }
+    //   ]
+    ,
       progress: 100
   },
   {
       projectName: "Landing Page Design",
       startDate: "04/6/2019",
       status: "Pending",
-      members: [
-          {
-              //name: "Design",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-2.jpg"
-          }
-      ],
-      progress: 78
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-2.jpg"
+    //   [
+    //       {
+    //           //name: "Design",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-2.jpg"
+    //       }
+    //   ],
+      ,progress: 78
   },
   {
       projectName: "Multipurpose Landing Template",
       startDate: "06/6/2019",
       status: "Completed",
-      members: [
-          {
-              //name: "Template",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-3.jpg"
-          }
-      ],
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-3.jpg"
+    //   [
+    //       {
+    //           //name: "Template",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-3.jpg"
+    //       }
+    //   ]
+      ,
       progress: 100
   },{
       projectName: "Blog Template Design",
       startDate: "07/5/2019",
       status: "Completed",
-      members: [
-          {
-              //name: "Design",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
-          }
-      ],
-      progress: 100
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
+    //   [
+    //       {
+    //           //name: "Design",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
+    //       }
+    //   ],
+      ,progress: 100
   },
   {
       projectName: "Brand Logo Design",
       startDate: "08/6/2019",
       status: "Pending",
-      members: [
-          {
-              //name: "Design",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-5.jpg"
-          }
-      ],
-      progress: 54
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-5.jpg"
+    //   [
+    //       {
+    //           //name: "Design",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-5.jpg"
+    //       }
+    //   ],
+      ,progress: 54
   },
   {
       projectName: "Redesign - Landing Page",
       startDate: "10/6/2019",
       status: "Pending",
-      members: [
-          {
-              //name: "Redesign",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-6.jpg"
-          }
-      ],
-      progress: 41
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-2.jpg"
+    //   [
+    //       {
+    //           //name: "Redesign",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-6.jpg"
+    //       }
+    //   ],
+      ,progress: 41
   },
   {
       projectName: "Redesign - Dashboard",
       startDate: "12/5/2019",
       status: "Completed",
-      members: [
-          {
-              //name: "Redesign",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-7.jpg"
-          }
-      ],
-      progress: 100
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-3.jpg"
+    //   [
+    //       {
+    //           //name: "Redesign",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-7.jpg"
+    //       }
+    //   ],
+      ,progress: 100
   },
   {
       projectName: "Landing Page Design",
       startDate: "13/6/2019",
       status: "Pending",
-      members: [
-          {
-              //name: "Design",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-8.jpg"
-          }
-      ],
-      progress: 84
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-4.jpg"
+    //   [
+    //       {
+    //           //name: "Design",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-8.jpg"
+    //       }
+    //   ],
+      ,progress: 84
   },
   {
       projectName: "Multipurpose Landing Template",
       startDate: "15/6/2019",
       status: "Completed",
-      members: [
-          {
-              //name: "Template",
-              avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-9.jpg"
-          }
-      ],
-      progress: 100
+      members: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-5.jpg"
+    //   [
+    //       {
+    //           //name: "Template",
+    //           avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-9.jpg"
+    //       }
+    //   ],
+      ,progress: 100
   }
 ]
 
@@ -130,23 +141,31 @@ function generateTable(table, data) {
   for (let element of data) {
     let row = table.insertRow();
     for (let key in element) {
-      if(Array.isArray(element[key])) {
-          //generateTable(table, element[key])
-      } else {
-        let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
-      } 
+        if(typeof element[key] === 'string' && element[key].includes('https://')) {
+            let cell = row.insertCell()
+            let image = document.createElement('img')
+            image.src = element[key]
+            cell.appendChild(image)
+        } else if(typeof element[key] === 'number') {
+            let cell = row.insertCell()
+            let div = document.createElement('div')
+            let innerDiv = document.createElement('div')
+            innerDiv.style.width = element[key] + "%";
+            div.appendChild(innerDiv)
+            cell.appendChild(div)
+        } else {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
     }
     let cell = row.insertCell();
     let newCell = row.insertCell(-1);
     let edit = document.createElement('button')
-    edit.innerText = 'Edit'
     edit.addEventListener('click', () => {
       console.log('Hello')
     })
     let remove = document.createElement('button')
-    remove.innerText = 'Delete'
     remove.addEventListener('click', () => {
       table.deleteRow(1)
     })
@@ -160,6 +179,7 @@ let data = Object.keys(Data[0]);
 generateTableHead(table, data);
 generateTable(table, Data);
   
+
 function addData(event) {
   const obj = {};
   event.preventDefault()
@@ -167,13 +187,8 @@ function addData(event) {
   obj.projectName = getData[0].value
   obj.startDate = getData[1].value
   obj.status = getData[2].value < 100 ? 'Pending' : 'Completed'
-  obj.members = [
-      {
-          //name: "Design",
-          avatarUrl: "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-1.jpg"
-      }
-  ]
-  obj.progress = getData[2].value
+  obj.members = "https://demos.creative-tim.com/argon-dashboard-pro/assets/img/theme/team-3.jpg"
+  obj.progress = parseInt(getData[2].value)
   Data.push(obj)
   data = Object.keys(Data[0]);
   generateTable(table, Data);
